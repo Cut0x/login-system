745b7bd4ea336b58bd1da5ec1853900416882849
Cut0x <71967731+Cut0x@users.noreply.github.com>
2022-06-22 23:47:50 +0200

    Update functionLogin.js

--- function/functionLogin.js
+++ function/functionLogin.js
@@ -17,10 +17,10 @@ function verifLogin() {
         if (input_name !== "admin") {
             document.getElementById("tested-false").innerHTML = "Le nom d'utilisateur est introuvable !";
         } else {
-            if (!input_email !== "admin@admin.com") {
+            if (input_email !== "admin@admin.com") {
                 document.getElementById("tested-false").innerHTML = "L'adresse mail ne correspond pas au nom d'utilisateur !";
             } else {
-                if (!input_password !== "123456") {
+                if (input_password !== "123456") {
                     document.getElementById("tested-false").innerHTML = "Le mot de passe est faux !";
                 } else {
                     document.getElementById("tested-true").innerHTML = "Connexion réussi !"
